67c3d91a2c136a87fa928c376ad75f2173729811
carlomagno-ale <carlomagno.ale@gmail.com>
2025-04-01 18:22:57 +0200

    Update About and PostsList pages with improved content and layout

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,14 +1,28 @@
 export default function About() {
 
     return (
-
         <>
-
             <main>
-                <div className="container text-center">
+                <div className="container">
 
-                    <h1>About us</h1>
-
+                    <section className="row align-items-center mt-5">
+                        <div className="col-md-6 text-start">
+                            <h1>About Us</h1>
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quisquam in nihil cupiditate quo? Cum molestiae dolores ad libero id fugiat possimus fugit, ipsum, eum iure, maiores eius odit beatae autem.
+                            </p>
+                            <p>
+                                Our mission is Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, nihil?
+                            </p>
+                        </div>
+                        <div className="col-md-6 text-end">
+                            <img
+                                src="https://picsum.photos/400/300"
+                                alt="About Us"
+                                className="img-fluid"
+                            />
+                        </div>
+                    </section>
                 </div>
             </main>
         </>
--- src/pages/PostsList.jsx
+++ src/pages/PostsList.jsx
@@ -5,52 +5,52 @@ export default function PostsList() {
         <>
             <main>
                 <div className="container">
-                    <div className="row">
+                    <div className="row mt-5">
                         <div className="col-md-4 mb-4">
                             <div className="card">
                                 <div className="card-body">
-                                    <h5 className="card-title">Card Title 1</h5>
-                                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    <h5 className="card-title">Post 1</h5>
+                                    <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, eos.</p>
                                 </div>
                             </div>
                         </div>
                         <div className="col-md-4 mb-4">
                             <div className="card">
                                 <div className="card-body">
-                                    <h5 className="card-title">Card Title 2</h5>
-                                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    <h5 className="card-title">Post 2</h5>
+                                    <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, eos.</p>
                                 </div>
                             </div>
                         </div>
                         <div className="col-md-4 mb-4">
                             <div className="card">
                                 <div className="card-body">
-                                    <h5 className="card-title">Card Title 3</h5>
-                                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    <h5 className="card-title">Post 3</h5>
+                                    <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, eos.</p>
                                 </div>
                             </div>
                         </div>
                         <div className="col-md-4 mb-4">
                             <div className="card">
                                 <div className="card-body">
-                                    <h5 className="card-title">Card Title 4</h5>
-                                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    <h5 className="card-title">Post 4</h5>
+                                    <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, eos.</p>
                                 </div>
                             </div>
                         </div>
                         <div className="col-md-4 mb-4">
                             <div className="card">
                                 <div className="card-body">
-                                    <h5 className="card-title">Card Title 5</h5>
-                                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    <h5 className="card-title">Post 5</h5>
+                                    <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, eos.</p>
                                 </div>
                             </div>
                         </div>
                         <div className="col-md-4 mb-4">
                             <div className="card">
                                 <div className="card-body">
-                                    <h5 className="card-title">Card Title 6</h5>
-                                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    <h5 className="card-title">Post 6</h5>
+                                    <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, eos.</p>
                                 </div>
                             </div>
                         </div>
