3b8b0a87af1a9c50eb137868ffcf0db9c91e8e98
carlomagno-ale <carlomagno.ale@gmail.com>
2025-04-01 16:04:24 +0200

    Add routing and new pages for Home, About, and PostsList

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
-function App() {
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './pages/Home'
+import About from "./pages/About";
+import PostsList from "./pages/PostsList";
+
+
+export default function App() {
 
 
   return (
     <>
 
+      <BrowserRouter>
+
+        <Routes>
+
+          <Route path="/" Component={Home} />
+          <Route path="/post" Component={PostsList} />
+          <Route path="/about" Component={About} />
+
+        </Routes>
+
+      </BrowserRouter>
+
     </>
   )
 }
-
-export default App
