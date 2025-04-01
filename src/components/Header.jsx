ba6d7ef44214446830a59d2ea974b90d002d70f1
carlomagno-ale <carlomagno.ale@gmail.com>
2025-04-01 18:13:51 +0200

    Enhance Home page layout and styling; update Header component for improved navigation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,14 +9,13 @@ export default function Header() {
                 <div className="container">
                     <nav className="navbar navbar-expand-lg navbar-light">
                         <div className="container-fluid">
-
                             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                                 <span className="navbar-toggler-icon"></span>
                             </button>
                             <div className="collapse navbar-collapse" id="navbarNav">
                                 <ul className="navbar-nav">
                                     <li className="nav-item">
-                                        <NavLink className="nav-link active" to="/">Home</NavLink>
+                                        <NavLink className="nav-link" to="/">Home</NavLink>
                                     </li>
                                     <li className="nav-item">
                                         <NavLink className="nav-link" to="/post">Posts</NavLink>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,12 +5,15 @@ export default function Home() {
 
         <>
             <main>
-                <div className="container text-center">
-
-                    <h1>home</h1>
-
+                <div className="jumbotron"></div>
+                <div className="container text-center mt-3">
+                    <h1 className="display-4 title">Welcome to the Home Page!</h1>
+                    <p className="lead">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis impedit qui deleniti.</p>
+                    <hr className="my-4" />
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui, sunt!</p>
                 </div>
-            </main>
+
+            </main >
         </>
     )
 }
