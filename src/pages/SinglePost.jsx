0c41cb16b3c1315373ce070f80df6fd4f9fb685e
carlomagno-ale <carlomagno.ale@gmail.com>
2025-04-02 17:13:04 +0200

    Update SinglePost component layout and styling for better post display

--- src/pages/SinglePost.jsx
+++ src/pages/SinglePost.jsx
@@ -4,10 +4,8 @@ import { useNavigate, useParams } from "react-router-dom";
 export default function SinglePost() {
 
     const [post, setPost] = useState()
-    //console.log(post.image)
 
     const { slug } = useParams()
-    //console.log(slug);
 
     const navigate = useNavigate();
 
@@ -27,33 +25,33 @@ export default function SinglePost() {
 
     }, [])
 
+    console.log(post)
+
     return (
         <>
             <main>
-                <div className="container">
+                <div className="container text-center">
 
-                    <div className="d-flex flex-column justify-content-center align-items-center">
-                        <h1 className="text-center my-3">{slug}</h1>
+                    {/*!post ? ('Caricamento')
+                    : (
+                        */}
 
-                        <button onClick={() => navigate(-1)}>
-                            Torna alla pagina precedente
-                        </button>
-                    </div>
+                    {post && (
+                        <div>
+                            <h1 className="my-3">{post.title}</h1>
+                            <img src={`http://localhost:3000${post.image}`} alt={post.slug} />
+                            <div className="mt-3">
+                                <p className="my-4">{post.content}</p>
+                            </div>
+                        </div>
+                    )}
+
+                    <button className="mt-2" onClick={() => navigate(-1)}>
+                        Torna alla pagina precedente
+                    </button>
+
                 </div>
             </main>
-
-            {/*
-
-                <div className="container text-center">
-                    <h1 className="my-3">{post.title}</h1>
-                    <img src={`http://localhost:3000${post.image}`} alt={post.slug} />
-                    <div className="mt-3">
-                        <p className="my-3">{post.content}</p>
-                    </div>
-                </div>
-
-            */}
-
         </>
     );
 }
