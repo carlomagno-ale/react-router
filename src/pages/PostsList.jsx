3d3ef716122c9e426913f035d0b6ae120c2d53f0
carlomagno-ale <carlomagno.ale@gmail.com>
2025-04-02 17:31:49 +0200

    Update layout and styling in PostsList component for improved card display

--- src/pages/PostsList.jsx
+++ src/pages/PostsList.jsx
@@ -23,17 +23,17 @@ export default function PostsList() {
         <>
             <main>
                 <div className="container">
-                    <div className="row mt-5">
+                    <div className="row mt-5 jus">
 
                         {posts.map(post => (
-                            <div className="col-4 mb-4" key={post.slug}>
+                            <div className="col-4 mb-4 " key={post.slug}>
                                 <div className="card">
                                     <div className="card-body">
                                         <Link to={`/post/${post.slug}`}>
                                             <img src={`http://localhost:3000${post.image}`} className="card-img-top" alt={post.slug} />
+                                            <h5 className="card-title fs-6 pt-3">{post.title}</h5>
+                                            <span className="card-text">Scopri la ricetta</span>
                                         </Link>
-                                        <h5 className="card-title fs-5">{post.title}</h5>
-                                        <p className="card-text">Scopri la ricetta</p>
                                     </div>
                                 </div>
                             </div>
